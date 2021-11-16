58beca67fc79acc6523d1c9038467fa1f3955538
Joshuapmb <joshuabradley99@gmail.com>
2021-11-16 20:58:50 +0000

    Added audios of different tinnitus, a button to play them and a method

--- src/app/components/the-office-page/the-office-page.component.ts
+++ src/app/components/the-office-page/the-office-page.component.ts
@@ -10,6 +10,39 @@ export class TheOfficePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+
+  audioPlaying = false;
+  audio = new Audio();
+  
+  toggleTinnitus(selectedSound: string){
+    
+    if (selectedSound == "4KhzTWave"){
+      this.audio.src = "../../../assets/4 Khz Triangle Wave.wav"
+    }
+    else if(selectedSound == "test"){
+      this.audio.src = "../../../assets/test.wav";
+    }
+
+    
+    this.audio.load();
+
+    if (this.audioPlaying == false){
+      this.audio.play();
+      this.audio.loop = true;
+      this.audioPlaying = true;
+      setTimeout(this.toggleTinnitus, 1500);
+    } 
+    else{
+      this.audio.loop = false;
+      this.audio.pause();
+      this.audioPlaying = false;
+    }
+
+
+  }
+
+
 
 }
