c323d01bbd8a4ed45d71bab44dfc460ab37171f0
Joshuapmb <joshuabradley99@gmail.com>
2021-11-18 16:59:59 +0000

    Added more tinnitus sounds, and when one is clicked, the rest are disabled
    New sounds need lengthening

--- src/app/components/the-office-page/the-office-page.component.ts
+++ src/app/components/the-office-page/the-office-page.component.ts
@@ -15,29 +15,85 @@ export class TheOfficePageComponent implements OnInit {
 
   audioPlaying = false;
   audio = new Audio();
-  
+  activeTinnitusButton : any;
+  arrayOfTinnitusButtons : any;
+
   toggleTinnitus(selectedSound: string){
     
     if (selectedSound == "4KhzTWave"){
       this.audio.src = "../../../assets/4 Khz Triangle Wave.wav"
+      this.activeTinnitusButton = document.getElementById("tinnitus4KhzTWave")
+    }
+    else if(selectedSound == "5KhzIWave"){
+      this.audio.src = "../../../assets/5 Khz Impulse Wave.wav";
+      this.activeTinnitusButton = document.getElementById("tinnitus5KhzIWave")
+    }
+    else if(selectedSound == "5KhzSWave"){
+      this.audio.src = "../../../assets/5 Khz Square Wave.wav";
+      this.activeTinnitusButton = document.getElementById("tinnitus5KhzSWave")
+    }
+    else if(selectedSound == "8KhzTWave"){
+      this.audio.src = "../../../assets/8 Khz Triangle Wave.wav";
+      this.activeTinnitusButton = document.getElementById("tinnitus8KhzTWave")
+    }
+    else if(selectedSound == "10KhzSinWave"){
+      this.audio.src = "../../../assets/10 Khz Sine Wave.wav";
+      this.activeTinnitusButton = document.getElementById("tinnitus10KhzSinWave")
+    }
+    else if(selectedSound == "13KhzSWave"){
+      this.audio.src = "../../../assets/13 Khz Square Wave.wav";
+      this.activeTinnitusButton = document.getElementById("tinnitus13KhzSWave")
+    }
+    else if(selectedSound == "14KhzSinWave"){
+      this.audio.src = "../../../assets/14 Khz Sine Wave.wav";
+      this.activeTinnitusButton = document.getElementById("tinnitus14KhzSinWave")
+    }
+    else if(selectedSound == "200hzIWave"){
+      this.audio.src = "../../../assets/200 Hz Impulse Wave.wav";
+      this.activeTinnitusButton = document.getElementById("tinnitus200hzIWave")
+    }
+    else if(selectedSound == "BrownNoise"){
+      this.audio.src = "../../../assets/Brown Noise.wav";
+      this.activeTinnitusButton = document.getElementById("tinnitusBrownNoise")
+    }
+    else if(selectedSound == "VioletNoise"){
+      this.audio.src = "../../../assets/Violet Noise.wav";
+      this.activeTinnitusButton = document.getElementById("tinnitusVioletNoise")
     }
     else if(selectedSound == "test"){
       this.audio.src = "../../../assets/test.wav";
+      this.activeTinnitusButton = document.getElementById("test")
     }
 
-    
+    this.arrayOfTinnitusButtons = document.getElementsByClassName("tinnitusButton")
     this.audio.load();
 
+    // When the audio is inactive
     if (this.audioPlaying == false){
+      // Start playing it in a loop
       this.audio.play();
       this.audio.loop = true;
       this.audioPlaying = true;
-      setTimeout(this.toggleTinnitus, 1500);
+
+      // Disable the other buttons by disabling all then re-enabling the active 
+      for (var i = 0 ; i<this.arrayOfTinnitusButtons.length; i++){
+        this.arrayOfTinnitusButtons[i].disabled = true 
+      }
+      this.activeTinnitusButton.disabled = false
     } 
+
+
+    // When the audio is already playing
     else{
+      // Turn it off
       this.audio.loop = false;
       this.audio.pause();
       this.audioPlaying = false;
+
+      // Re-enable the other buttons
+      for (var i = 0 ; i<this.arrayOfTinnitusButtons.length; i++){
+      this.arrayOfTinnitusButtons[i].disabled = false 
+      }
     }
 
 
